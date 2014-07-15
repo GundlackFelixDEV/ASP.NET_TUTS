eb78abcb05e37df601cd93424c1e5a4f8de081bd
GundlackFelixDEV <GundlackFelixDEV@gmail.com>
2014-07-15 21:05:46 +0200

    Mobile/Desktop Changer added

--- EventLogger/EventLogger/Global.asax.cs
+++ EventLogger/EventLogger/Global.asax.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.UI.WebControls;
+using System.Web.WebPages;
 
 namespace EventLogger
 {
