e917b66726501deb2e9c1af85270f3f0cd702ab0
Felix Gundlack <GundlackFelixDEV@gmail.com>
2014-10-06 21:15:34 +0200

    Added click event and initial local position

--- GuessGPSApp/js/Controller/GeolocationController.js
+++ GuessGPSApp/js/Controller/GeolocationController.js
@@ -1,46 +1,88 @@
 GeoGuessApp.controller('GeolocationController',function($scope){
 
 	//Geolocation Model
-	$scope.Position = {
-		Geolocation: {},
-		Latitude: 0,
-		Longitude: 1000
-	};
+	$scope.Position = {};
 	$scope.ErrorMessage = "";
-	
-	//Geolocation Functions
-	var nav = null;
-	$scope.GetCurrentPosition = function(){
-		if(nav == null){
+	var map = null;
+        var marker = null;
+        $scope.MapsOption = {
+            zoom: 13
+        };
+        
+        $scope.InitGoogleMaps = function(position){           
+           console.log("InitGoogleMaps");
+           if(!position.coords)
+           {
+               $scope.Error("Unable to initialize google maps widget. Position unknown");
+               return;
+           }
+           var pos = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+           var options = $scope.MapsOption;
+           options.center = pos;
+           
+           if(map === null){
+               map = new google.maps.Map(document.getElementById('map-canvas'), options);
+           }
+           
+           if(marker === null){
+            marker = new google.maps.Marker({
+                position: pos,
+                map: map,
+                title: 'Current Position!'
+            }); 
+           }              
+            
+           google.maps.event.addListener(map, 'click', function(event){
+               $scope.HandleMapClick(event.latLng);
+           });
+        };
+        $scope.HandleMapClick = function(location){
+            console.log("HandleMapClick");
+            var pos = { coords: {
+                    latitude: location.lat(),
+                    longitude: location.lng()}};
+            $scope.SetPosition(pos);
+        };
+            
+    	//Geolocation Functions
+	var nav = null;  
+	$scope.GetCurrentPosition = function(){                
+		if(nav === null){
 			nav = window.navigator;
 		}
-		if(nav != null){
+		if(nav !== null){
 			if(nav.geolocation){
-				nav.geolocation.getCurrentPosition($scope.SetPosition,$scope.GeolocationErrorCallback);
-			} else {
+				nav.geolocation.getCurrentPosition(function(pos){
+                                    $scope.SetPosition(pos);
+                                },$scope.GeolocationErrorCallback);
+                                
+                        } else {
 				$scope.Error("Geolocation is not supported by this browser.");
 			}
 		}
 		else {
 			$scope.Error("Unable to find navigator");
 		}
+                return null;
 	};
 	
-	$scope.SetPosition = function(position,event){
+	$scope.SetPosition = function(position){
 		console.log("setting position:" + position);
-		$scope.Position.Geolocation = position;
-		$scope.Position.Latitude = position.coords.latitude;
-		$scope.Position.Longitude = position.coords.longitude;
+		$scope.Position = position;
+                if(map === null){
+                    $scope.InitGoogleMaps(position);
+                }
+                marker.setPosition(new google.maps.LatLng(position.coords.longitude,position.coords.latitude)); 
 		$scope.$apply();
 	};
 	
 	$scope.Error = function(message){
-		if(!message || message.length == 0){
+		if(!message || message.length === 0){
 			message = "Unknown Error";
 		}
 		console.log("GeolocationController: " + message);
 		$scope.ErrorMessage = message;
-		$scope.$apply();
+		//$scope.$apply();
 	};
 	
 	$scope.GeolocationErrorCallback = function(error){
@@ -70,4 +112,6 @@ GeoGuessApp.controller('GeolocationController',function($scope){
 	    }
 		$scope.Error(message);
 	};
+        
+        google.maps.event.addDomListener(window, 'load', $scope.GetCurrentPosition());
 });
