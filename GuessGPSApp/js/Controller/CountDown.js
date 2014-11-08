04dacd4a6dd03aad4937d1adf7617e72d011c041
Felix Gundlack <GundlackFelixDEV@gmail.com>
2014-11-08 10:37:41 +0100

    CountDown rewritten to handle insert functionality int $scope.CountDown

--- GuessGPSApp/js/Controller/CountDown.js
+++ GuessGPSApp/js/Controller/CountDown.js
@@ -4,74 +4,76 @@
  * and open the template in the editor.
  */
 function CountDown($scope,$timeout,CountDownService){
-    $scope.Start= function(){
-        console.log("CountDown.Start");
-        if(!$scope.IsRunning()){
-              $scope.Timer = $timeout(CountDown,Math.abs($scope.Options.delta),true);
-              this.Status = "Start";
+    $scope.CountDown = {
+        Start:  function(){
+                console.log("CountDown.Start");
+                if(!$scope.CountDown.IsRunning()){
+                      $scope.Timer = $timeout(CountDown,Math.abs($scope.CountDown.Options.delta),true);
+                      this.Status = "Start";
+                }
+        },
+        Stop: function(){
+            console.log("CountDown.Stop");
+            $scope.CountDown.Pause();
+            this.Status = "Stop";
+            $scope.T = $scope.CountDown.Options.T_Start;
+        },
+        Pause: function(){
+            console.log("CountDown.Pause");
+            $timeout.cancel(this.Timer);
+            $scope.CountDown.Timer = null;
+            this.Status = "Pause";
         }
     };
-    $scope.Stop = function(){
-        console.log("CountDown.Stop");
-        $scope.Pause();
-	this.Status = "Stop";
-        $scope.T = $scope.Options.T_Start;
-
-    };
-    $scope.Pause = function(){
-        console.log("CountDown.Pause");
-        $timeout.cancel(this.Timer);
-        $scope.Timer = null;
-        this.Status = "Pause";
-    };
     var CountDown = function(){
-       var opt = $scope.Options;
-       if((opt.delta > 0 && $scope.T < opt.T_Stop) ||
-            (opt.delta < 0 && $scope.T > opt.T_Stop)){
-                $scope.T += opt.delta;
-                $scope.Timer = $timeout(CountDown,Math.abs(opt.delta),true);
+       var opt = $scope.CountDown.Options;
+       if((opt.delta > 0 && $scope.CountDown.T < opt.T_Stop) ||
+            (opt.delta < 0 && $scope.CountDown.T > opt.T_Stop)){
+                $scope.CountDown.T += opt.delta;
+                $scope.CountDown.Timer = $timeout(CountDown,Math.abs(opt.delta),true);
             }
     };
-    $scope.SetOptions = function(opt){
+    $scope.CountDown.SetOptions = function(opt){
         console.log("CountDown.SetOptions");
-        $scope.Pause();
+        $scope.CountDown.Pause();
         if(opt instanceof CountDownOpts)
         {
-            $scope.Options = opt;
+            $scope.CountDown.Options = opt;
         }else{
             console.error("CountDown.SetOptions unrecogniced option format. Standard initialization of CountDownOpts will be used!");
-            $scope.Options = new CountDownOpts();
+            $scope.CountDown.Options = new CountDownOpts();
         } 
-        $scope.T = $scope.Options.T_Start;
+        $scope.CountDown.T = $scope.CountDown.Options.T_Start;
     };
     $scope.$on("Start",function(){
         console.log("CountDown.on: Start");
-        $scope.SetOptions(CountDownService.options);
-        $scope.Start();
+        $scope.CountDown.SetOptions(CountDownService.options);
+        $scope.CountDown.Start();
     });
     $scope.$on("Stop",function(){
         console.log("CountDown.on: Stop");
-        $scope.Stop();
+        $scope.CountDown.Stop();
     });
     $scope.$on("Pause",function(){
         console.log("CountDown.on: Pause");
-        $scope.Pause();
+        $scope.CountDown.Pause();
     });
-    $scope.$watch(function(scope){return scope.T;},function(newValue){
+    $scope.$watch(function(scope){return scope.CountDown.T;},function(newValue){
         console.log(newValue);
-        if(newValue === $scope.Options.T_Stop){
+        if(newValue === $scope.CountDown.Options.T_Stop){
             CountDownService.CountDownEnd();
         }else if(newValue <= 5000){
             $("#CountDownDisplay").css({fontSize:"2em"}).animate({fontSize: "3.5em"}, 500).delay(500).css({fontSize:"2em"});
         }
     });	
-    $scope.IsRunning = function(){
+    $scope.CountDown.IsRunning = function(){
             return (this.Status === "Start");
     };
+    
     this.Continue = false;
-    $scope.Timer = null;
     this.Status = "Stop";
-    $scope.SetOptions($scope.Options);
+    $scope.CountDown.Timer = null;
+    $scope.CountDown.SetOptions($scope.CountDown.Options);
 
 };
 
